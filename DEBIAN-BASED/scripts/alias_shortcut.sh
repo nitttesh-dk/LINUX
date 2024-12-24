40fb2ac405d9d51eb82f23aa0726918a50fa9ff6
nitttesh-dk <niteshdk11@gmail.com>
2024-12-24 15:54:50 +0530

    Update alias_shortcut.sh

--- DEBIAN-BASED/scripts/alias_shortcut.sh
+++ DEBIAN-BASED/scripts/alias_shortcut.sh
@@ -4,3 +4,8 @@ alias sz='source ~/.zshrc'
 alias fs="yazi"
 alias codei="code-insiders"
 alias clrr="history -c"
+
+
+# brew install fzf fd  bat git-delta eza tldr zoxide
+
+# git clone https://github.com/junegunn/fzf-git.sh.git
